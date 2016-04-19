c9a66bff40eda91b995a6aed98abd0f72519114f
j0y <hope2013@mail.ru>
2016-04-19 10:02:22 +0700

    Initial commit

---
+++ syllables.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+import re
+vowels = set(u'аеёиоуыэюя')
+sign_chars = set(u'ъь')
+pattern = re.compile(u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))")
+
+def get_syllables(word):
+    mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
+    return [word[m.start():m.end()] for m in pattern.finditer(mask)]
+
+word = raw_input('слово:').decode('utf-8')
+print '-'.join(get_syllables(word))
