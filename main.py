dab973a18be994366cb630bfcca4ee355182170c
j0y <hope2013@mail.ru>
2016-04-19 16:47:28 +0700

    Get comments

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pikabu
 import credentials
 
+import re
+def striphtml(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
 api = pikabu.Api(login=credentials.login, password=credentials.password)
-posts = api.posts.get("new",0)
-for post in posts:
-	print post.id
+posts = api.posts.get("hot",0)
+#for post in posts:
+#   print post.id
+
+comments = api.comments.get(posts[0].id)
+for comment in comments:
+    if len(comment.text) < 100:
+        print striphtml(comment.text)
+
+#TODO: Check for minimum 3 words, then check if first word have 5 syllables ens with whole word, ten second, then third
