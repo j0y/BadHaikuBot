da7b9bae5cb8800c3ca65dc6e5d67c4441210b6d
j0y <hope2013@mail.ru>
2016-04-25 13:20:29 +0700

    Fixed disappearing letters in regex

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 vowels = set(u'аеёиоуыэюя')
 sign_chars = set(u'ъь')
-pattern = re.compile(u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=$))")
+pattern = re.compile(u"(c*[ьъ]?vc+[ьъ](?=v))|(c*[ьъ]?v(?=v|cv))|(c*[ьъ]?vc[ъь]?(?=cv|ccv))|(c*[ьъ]?v[cьъ]*(?=c|v)?)")
 
 def get_syllables(word):
     mask = ''.join(['v' if c in vowels else c if c in sign_chars else 'c' for c in word.lower()])
